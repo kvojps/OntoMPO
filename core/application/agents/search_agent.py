3101c913f7e3d27ed78daaa7dfb377a0dac0c22c
jose <josebazjunior09@gmail.com>
2024-12-11 22:41:20 -0300

    [feat]: enhance SearchAgent to include type hints and improve hierarchy structure

--- core/application/agents/search_agent.py
+++ core/application/agents/search_agent.py
@@ -1,3 +1,4 @@
+from typing import Any, Optional, Tuple
 from core.infrastructure.ontology.repository.ontology_repository import (
     OntologyRepository,
 )
@@ -7,16 +8,26 @@ class SearchAgent:
     def __init__(self, ontology_repository: OntologyRepository):
         self.ontology_repository = ontology_repository
 
-    def get_mpo_process_layer_definition(self):
-        mpo_process_main_classes, error = self.ontology_repository.get_subclasses("Process")
+    def get_mpo_process_layer_definition(
+        self,
+    ) -> Tuple[Optional[list[dict[str, Any]]], Optional[str]]:
+        mpo_process_main_classes, error = self.ontology_repository.get_subclasses(
+            "Process"
+        )
         if error:
             return None, error
         assert mpo_process_main_classes is not None
 
-        mpo_process_hierarchy = [self._build_hierarchy(cls) for cls in mpo_process_main_classes]
+        mpo_process_hierarchy = [
+            self._build_hierarchy(cls) for cls in mpo_process_main_classes
+        ]
 
         return mpo_process_hierarchy, None
 
-    
-    def _build_hierarchy(self, cls):
-        return {cls.name: [self._build_hierarchy(subcls) for subcls in cls.subclasses()]}
+    def _build_hierarchy(self, cls) -> dict[str, Any]:
+        return {
+            "class_name": cls.name,
+            "subclasses": [
+                self._build_hierarchy(subcls) for subcls in cls.subclasses()
+            ],
+        }
